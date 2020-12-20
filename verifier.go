fb3292bb0c49b1818112936a020aa0519f39c1f0
Jim Lambert <jlambert@hashicorp.com>
2020-12-19 19:26:30 -0500

    generate crypographically random bytes that meet the requirements for a code verifier from rfc7636

--- verifier.go
+++ verifier.go
@@ -1,12 +1,12 @@
 package go_oauth_pkce_code_verifier
 
 import (
+	"crypto/rand"
 	"crypto/sha256"
 	"encoding/base64"
 	"fmt"
-	"math/rand"
+	"io"
 	"strings"
-	"time"
 )
 
 const (
@@ -27,12 +27,11 @@ func CreateCodeVerifierWithLength(length int) (*CodeVerifier, error) {
 	if length < MinLength || length > MaxLength {
 		return nil, fmt.Errorf("invalid length: %v", length)
 	}
-	r := rand.New(rand.NewSource(time.Now().UnixNano()))
-	b := make([]byte, length, length)
-	for i := 0; i < length; i++ {
-		b[i] = byte(r.Intn(255))
+	buf, err := randomBytes(length)
+	if err != nil {
+		return nil, fmt.Errorf("failed to generate random bytes: %v", err)
 	}
-	return CreateCodeVerifierFromBytes(b)
+	return CreateCodeVerifierFromBytes(buf)
 }
 
 func CreateCodeVerifierFromBytes(b []byte) (*CodeVerifier, error) {
@@ -61,4 +60,28 @@ func encode(msg []byte) string {
 	encoded = strings.Replace(encoded, "/", "_", -1)
 	encoded = strings.Replace(encoded, "=", "", -1)
 	return encoded
+}
+
+// https://tools.ietf.org/html/rfc7636#section-4.1)
+func randomBytes(length int) ([]byte, error) {
+	const charset = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
+	const csLen = byte(len(charset))
+	output := make([]byte, 0, length)
+	for {
+		buf := make([]byte, length)
+		if _, err := io.ReadFull(rand.Reader, buf); err != nil {
+			return nil, fmt.Errorf("failed to read random bytes: %v", err)
+		}
+		for _, b := range buf {
+			// Avoid bias by using a value range that's a multiple of 62
+			if b < (csLen * 4) {
+				output = append(output, charset[b%csLen])
+
+				if len(output) == length {
+					return output, nil
+				}
+			}
+		}
+	}
+
 }
